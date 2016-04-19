859cc083c6241dd077c3323b3e792c442418ac2d
Luis J <jarolu@gmail.com>
2016-04-19 20:13:24 +0200

    Test un numero devuelve el numero

--- src/net/iessanclemente/dapw/katas/stringcalculator/StringCalculator.java
+++ src/net/iessanclemente/dapw/katas/stringcalculator/StringCalculator.java
@@ -2,7 +2,10 @@ package net.iessanclemente.dapw.katas.stringcalculator;
 
 public class StringCalculator {
 
-	public int add(String string) {
+	public int add(String txt) {
+		if(txt.length()>0){
+			return Integer.parseInt(txt);
+		}
 		return 0;
 	}
 
