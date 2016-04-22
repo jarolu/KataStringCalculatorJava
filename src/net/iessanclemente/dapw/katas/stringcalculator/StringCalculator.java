84443c03964f97a8a05bfafe911de881ddacf662
Luis J <jarolu@gmail.com>
2016-04-22 19:13:35 +0200

    Agregado separador personalizado

--- src/net/iessanclemente/dapw/katas/stringcalculator/StringCalculator.java
+++ src/net/iessanclemente/dapw/katas/stringcalculator/StringCalculator.java
@@ -5,8 +5,14 @@ public class StringCalculator {
 	public int add(String txt) {
 		
 		int result=0;
-		if(txt.length()>=1){							
-			String [] numberList=txt.split(",|\n");
+		if(txt.length()>=1){
+			String separators=",|\n";
+			if(txt.startsWith("//")){
+				String customSeparator=txt.substring(2, 3);
+				txt=txt.substring(4);
+				separators+="|"+customSeparator;				
+			}
+			String [] numberList=txt.split(separators);
 			for (String number : numberList) {
 				result+=Integer.parseInt(number);
 			}								
--- src/net/iessanclemente/dapw/katas/stringcalculator/tests/TestStringCalculator.java
+++ src/net/iessanclemente/dapw/katas/stringcalculator/tests/TestStringCalculator.java
@@ -17,37 +17,60 @@ public class TestStringCalculator {
 
 	@Test
 	public void testEmptyStringReturnsCero() {
-		int result=sc.add("");
-		assertEquals(0, result);	
+		int result = sc.add("");
+		assertEquals(0, result);
 	}
-	
+
 	@Test
 	public void testANumberReturnsTheNumber() {
-		int shouldBeOne=sc.add("1");
-		int shouldBeTwo=sc.add("2");
-		assertEquals(1, shouldBeOne);	
+		int shouldBeOne = sc.add("1");
+		int shouldBeTwo = sc.add("2");
+		assertEquals(1, shouldBeOne);
 		assertEquals(2, shouldBeTwo);
 	}
-	
+
 	@Test
 	public void testTwoNumbersReturnsTheSumOfThem() {
-		int shouldBeThree=sc.add("1,2");		
-		assertEquals(3, shouldBeThree);			
+		int shouldBeThree = sc.add("1,2");
+		assertEquals(3, shouldBeThree);
 	}
-	
-	
+
 	@Test
 	public void testManyNumbersReturnsTheSumOfThem() {
-		int shouldBeSix=sc.add("1,2,3");		
-		int shouldBeTen=sc.add("1,2,3,4");
-		assertEquals(6, shouldBeSix);			
+		int shouldBeSix = sc.add("1,2,3");
+		int shouldBeTen = sc.add("1,2,3,4");
+		assertEquals(6, shouldBeSix);
 		assertEquals(10, shouldBeTen);
 	}
-	
+
 	@Test
 	public void testCRSepator() {
-		int shouldBeThree=sc.add("1\n2");		
-		assertEquals(3, shouldBeThree);			
+		int shouldBeThree = sc.add("1\n2");
+		assertEquals(3, shouldBeThree);
+	}
+
+	@Test
+	public void testCRAndColonSepator() {
+		int shouldBeSix = sc.add("1\n2,3");
+		assertEquals(6, shouldBeSix);
+	}
+
+/*	@Test
+	public void testContiguousSepatorDoenstWork() {
+		NumberFormatException lanzada = null;
+		try {
+			sc.add("1\n,3");
+		} catch (NumberFormatException e) {
+			lanzada = e;
+		}
+		assertNotNull(lanzada);
+	}
+	*/
+	
+	@Test
+	public void testCustomSepartors() {
+		int shouldBeSix = sc.add("//@\n1@2@3");
+		assertEquals(6, shouldBeSix);
 	}
 
 }
